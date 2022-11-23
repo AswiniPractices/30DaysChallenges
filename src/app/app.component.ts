f2523be9b81632e2b3b926a0ba0cce8de9cab6ae
AswiniThota <aswinithota25@gmail.com>
2022-11-23 15:35:16 +0530

    ngx-pagination using div and table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ITable } from './models/ITable';
+import { AuthService } from './services/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'challenges';
+  data!: ITable[];
+  p: number = 1;
+  q: number = 1;
+  constructor(private auth:AuthService){}
+
+  ngOnInit() {
+    this.getTableData()
+  }
+
+  getTableData() {
+    this.auth.getData().subscribe((response:any) => {
+      return this.data = response;
+    })
+  }
 }
---
+++ src/app/models/table.ts
@@ -0,0 +1,8 @@
+
+export interface Table {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
